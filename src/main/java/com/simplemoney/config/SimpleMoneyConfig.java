aabbfbfcb2833ad8cf5c98a6e2f5c5018fcfa45c
Oussama Azarfane <62378600+Ouzzi@users.noreply.github.com>
2025-12-07 01:24:20 +0100

    some fixes, tweaks removewd and new mod created (simpletweaks)

--- src/main/java/com/simplemoney/config/SimpleMoneyConfig.java
+++ src/main/java/com/simplemoney/config/SimpleMoneyConfig.java
@@ -11,28 +11,11 @@ public class SimpleMoneyConfig implements ConfigData {
     @ConfigEntry.Gui.CollapsibleObject
     public Trades trades = new Trades();
 
-    @ConfigEntry.Gui.CollapsibleObject
-    public Balancing balancing = new Balancing();
-
-    @ConfigEntry.Gui.CollapsibleObject
-    public Vaults vaults = new Vaults();
-
     public static class Trades {
         @ConfigEntry.Gui.Tooltip // Keine Argumente nötig, sucht automatisch in .json
         public boolean enableVillagerTrades = true;
 
         @ConfigEntry.Gui.Tooltip
         public boolean enableWanderingTrades = true;
-    }
-
-    public static class Balancing {
-        // count = 2 bedeutet, der Tooltip hat 2 Zeilen in der .json
-        @ConfigEntry.Gui.Tooltip(count = 2)
-        public int rocketStackSize = 16;
-    }
-
-    public static class Vaults {
-        @ConfigEntry.Gui.Tooltip
-        public int vaultCooldownDays = 100;
     }
 }
